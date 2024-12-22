15bd6218b253fa6f29e4075a9e5c9a7baac36414
AYSE PARLA YERLI <ayse.yerli@lcwaikiki.com>
2024-12-22 22:04:48 +0300

    Add project files.

---
+++ WebApp.Command/Commands/CreatePdfActionCommand.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Command.Commands
+{
+    public class CreatePdfActionCommand<T> : ITableActionCommand
+    {
+        private readonly PdfFile<T> _pdfFile;
+
+        public CreatePdfActionCommand(PdfFile<T> pdfFile)
+        {
+            _pdfFile = pdfFile;
+        }
+
+        public IActionResult Execute()
+        {
+            var excelMemoryStream = _pdfFile.Create();
+
+            return new FileContentResult(excelMemoryStream.ToArray(), _pdfFile.FileType) { FileDownloadName = _pdfFile.FileName };
+        }
+    }
+}
